35a49ab84970185b2560b5171bb86a3305a57acd
Atreya Singh Sengar <2k23.csai2310583@gmail.com>
2026-09-05 21:30:32 +0530

    Time: 5 ms (72.65%), Space: 42.6 MB (99.71%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        Arrays.sort(citations);
+        int hindex=0;
+        for(int i=0;i<n;i++){
+            int h=n-i;
+            if(citations[i]>=h){
+                hindex=h;
+                break;
+            }
+        }
+        return hindex;
+    }
+}
